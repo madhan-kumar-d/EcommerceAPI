0f79bec2835947d99a592b3ed5e2bee4ed012ce8
Madhan kumar <madhank34@gmail.com>
2024-08-07 07:48:06 +0530

    Swagger Document 1st commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { serve, setup } from 'swagger-ui-express';
 import authRouter from './auth';
 import productRoute from './product';
 import cartRouter from './cart';
 import checkoutRouter from './checkout';
+import * as swaggerDoc from '../swagger/swaggerDocs.json';
 
 const mainRouter: Router = Router();
 
@@ -10,4 +12,5 @@ mainRouter.use('/auth', authRouter);
 mainRouter.use('/product', productRoute);
 mainRouter.use('/cart', cartRouter);
 mainRouter.use('/checkout', checkoutRouter);
+mainRouter.use('/api-docs', serve, setup(swaggerDoc));
 export default mainRouter;
